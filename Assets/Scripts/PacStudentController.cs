1b97314004336d27d3de26edc5d8bf1db08d92e8
IskandarMuminovv <Iskandar.Muminov-1@student.uts.edu.au>
2023-10-31 21:08:23 +1100

    simple movement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private LevelGenerator levelMap;
+    [SerializeField] private Animator animator;
+    private Input lastInput;
+    private Input currentInput;
+   
+
+    private void Start()
     {
-        
+        levelMap = FindObjectOfType<LevelGenerator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
     }
 }
+
