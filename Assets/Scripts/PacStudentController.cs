8b1b669ef6e9e59cd5ceb7e20f21f776b12233fe
IskandarMuminovv <Iskandar.Muminov-1@student.uts.edu.au>
2023-11-03 13:05:15 +1100

    failed attempt to make lerp movement based on the grid

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
-    [SerializeField] private LevelGenerator levelMap; 
 
-    private Vector3 targetPosition;
-    private Vector3 currentInput;
-    private Vector3 lastInput;
-    private bool isMoving = false;
+    [SerializeField] private Animator animator;
+
+    private Vector3 inputDirection = Vector3.zero;
+    private Vector3 movementDirection = Vector3.zero;
 
     private void Start()
     {
-        targetPosition = transform.position;
-        lastInput = Vector3.zero;
+        
     }
 
     private void Update()
     {
         HandleInput();
-        MovePacStudent();
+        MoveCharacter();
     }
 
     private void HandleInput()
@@ -28,62 +26,20 @@ public class PacStudentController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        currentInput = new Vector3(horizontalInput, verticalInput, 0).normalized;
-
-        if (currentInput != Vector3.zero)
-        {
-            lastInput = currentInput;
-        }
+        inputDirection = new Vector3(horizontalInput, verticalInput, 0).normalized;
     }
 
-    private void MovePacStudent()
+    private void MoveCharacter()
     {
-        if (!isMoving)
+        if (inputDirection != Vector3.zero)
         {
-            if (currentInput != Vector3.zero && IsWalkable(transform.position + currentInput))
-            {
-                targetPosition = transform.position + currentInput;
-                isMoving = true;
-            }
-            else if (IsWalkable(transform.position + lastInput))
-            {
-                targetPosition = transform.position + lastInput;
-                isMoving = true;
-            }
+            movementDirection = inputDirection;
         }
 
-        if (isMoving)
-        {
-            float step = moveSpeed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+        Vector3 movement = movementDirection * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
 
-            if (transform.position == targetPosition)
-            {
-                isMoving = false;
-            }
-        }
-    }
-
-    private bool IsWalkable(Vector3 position)
-    {
-        
-        Vector3Int gridPosition = levelMap.WorldToGridPosition(position);
-
-        if (IsInBounds(gridPosition))
-        {
-            int cellValue = levelMap.mapData[gridPosition.y, gridPosition.x];
-            return cellValue != 1 && cellValue != 2 && cellValue != 3 && cellValue != 4 && cellValue != 7;
-        }
-
-        return false;
-    }
-
-    private bool IsInBounds(Vector3Int gridPosition)
-    {
-        int maxY = levelMap.mapData.GetLength(0);
-        int maxX = levelMap.mapData.GetLength(1);
-
-        return gridPosition.y >= 0 && gridPosition.y < maxY &&
-               gridPosition.x >= 0 && gridPosition.x < maxX;
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
     }
 }
