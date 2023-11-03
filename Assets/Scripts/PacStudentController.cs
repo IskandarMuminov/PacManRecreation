74168d35098909b6881a6e0c1f2b0fe57b9ad605
IskandarMuminovv <Iskandar.Muminov-1@student.uts.edu.au>
2023-11-03 12:39:41 +1100

    attempts to do lerp grid based movement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,26 +1,26 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
-    [SerializeField] private LevelGenerator levelMap;
-    [SerializeField] private Animator animator;
+    [SerializeField] private LevelGenerator levelMap; 
 
-    private Vector3 inputDirection = Vector3.zero;
-    private Vector3 movementDirection = Vector3.zero;
+    private Vector3 targetPosition;
+    private Vector3 currentInput;
+    private Vector3 lastInput;
+    private bool isMoving = false;
 
     private void Start()
     {
-        levelMap = FindObjectOfType<LevelGenerator>();
+        targetPosition = transform.position;
+        lastInput = Vector3.zero;
     }
 
     private void Update()
     {
         HandleInput();
-        MoveCharacter();
+        MovePacStudent();
     }
 
     private void HandleInput()
@@ -28,23 +28,62 @@ public class PacStudentController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        inputDirection = new Vector3(horizontalInput, verticalInput, 0).normalized;
+        currentInput = new Vector3(horizontalInput, verticalInput, 0).normalized;
+
+        if (currentInput != Vector3.zero)
+        {
+            lastInput = currentInput;
+        }
     }
 
-    private void MoveCharacter()
+    private void MovePacStudent()
     {
-       
-        if (inputDirection != Vector3.zero)
+        if (!isMoving)
         {
-            movementDirection = inputDirection;
+            if (currentInput != Vector3.zero && IsWalkable(transform.position + currentInput))
+            {
+                targetPosition = transform.position + currentInput;
+                isMoving = true;
+            }
+            else if (IsWalkable(transform.position + lastInput))
+            {
+                targetPosition = transform.position + lastInput;
+                isMoving = true;
+            }
         }
 
-        Vector3 movement = movementDirection * moveSpeed * Time.deltaTime;
-        transform.Translate(movement);
+        if (isMoving)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
 
-       
-        animator.SetFloat("Horizontal", movementDirection.x);
-        animator.SetFloat("Vertical", movementDirection.y);
+            if (transform.position == targetPosition)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+    private bool IsWalkable(Vector3 position)
+    {
+        
+        Vector3Int gridPosition = levelMap.WorldToGridPosition(position);
+
+        if (IsInBounds(gridPosition))
+        {
+            int cellValue = levelMap.mapData[gridPosition.y, gridPosition.x];
+            return cellValue != 1 && cellValue != 2 && cellValue != 3 && cellValue != 4 && cellValue != 7;
+        }
+
+        return false;
+    }
+
+    private bool IsInBounds(Vector3Int gridPosition)
+    {
+        int maxY = levelMap.mapData.GetLength(0);
+        int maxX = levelMap.mapData.GetLength(1);
+
+        return gridPosition.y >= 0 && gridPosition.y < maxY &&
+               gridPosition.x >= 0 && gridPosition.x < maxX;
     }
 }
-
